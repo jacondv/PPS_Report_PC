01b267f45faf25b9e012b7f91088d242df0059be
vohoaidanh <danhvohoai2@gmail.com>
2026-04-13 21:44:38 +0700

    update teamplate

--- report/__init__.py
+++ report/__init__.py
@@ -2,6 +2,11 @@
 Report generation modules.
 """
 
-from report.core.pdf_generator import PDFGenerator
+from report.core.html_pdf_generator import HTMLPDFGenerator
+from report.core.pdf_generator import PDFGenerator as LegacyPDFGenerator
+
+PDFGenerator = HTMLPDFGenerator
+
+__all__ = ['PDFGenerator', 'HTMLPDFGenerator', 'LegacyPDFGenerator']
 
 __all__ = ['PDFGenerator']
